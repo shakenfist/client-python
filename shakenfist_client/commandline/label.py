2fa1140ce56e5573de28f6a832a8936cf434cffc
Michael Still <mikal@stillhq.com>
2021-09-15 20:03:37 +1000

    Add support for updating a label.

---
+++ shakenfist_client/commandline/label.py
@@ -0,0 +1,17 @@
+import click
+
+
+@click.group(help='Label commands')
+def label():
+    pass
+
+
+@label.command(name='update',
+               help=('Update a label to use a new blob.\n\n'
+                     'LABEL:     The name of the label to update\n'
+                     'BLOB_UUID: The UUID of the blob to use.'))
+@click.argument('label', type=click.STRING)
+@click.argument('blob_uuid', type=click.STRING)
+@click.pass_context
+def label_update(ctx, label, blob_uuid=None):
+    ctx.obj['CLIENT'].update_label(label, blob_uuid)
