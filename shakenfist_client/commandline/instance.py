3908e68d1340074a8bfdaf7bd0d53f7b2ec46357
Michael Still <mikal@stillhq.com>
2024-10-02 18:14:16 +1000

    Fix the lint fixes for instance.py

--- shakenfist_client/commandline/instance.py
+++ shakenfist_client/commandline/instance.py
@@ -62,8 +62,8 @@ def instance_list(ctx, all=False):
         for i in insts:
             ifaces = []
             for interface in _get_interfaces(ctx, i):
-                iface = (f'{interface['order']}: '
-                         f'{interface.get('ipv4', 'No address assigned')}')
+                iface = (f"{interface['order']}: "
+                         f"{interface.get('ipv4', 'No address assigned')}")
                 if interface.get('floating'):
                     iface += ' ({})'.format(interface['floating'])
                 ifaces.append(iface)
